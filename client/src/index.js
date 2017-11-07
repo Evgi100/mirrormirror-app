c685c13738948635f644b76b152b206508100e2c
Andrea Selinger <Seli@MacBook-Pro-4.local>
2017-11-07 19:42:28 +0200

    navbar added

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from './navbar.js';
 
 class App extends React.Component {
     constructor(props) {
@@ -8,7 +9,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello World</h1>
+          <Navbar/>
             </div>
         );
     }
