3325d8d571014d568ef2af5e19fe5a041b79917f
Evgi100 <evgeni.shapira@gmail.com>
2017-11-08 15:00:17 +0200

    fixed the git merge

--- client/src/UsersList.js
+++ client/src/UsersList.js
@@ -3,11 +3,17 @@ import UserBox from './userBox';
 class UsersListBox extends React.Component {
     constructor(props) {
         super(props);
+        this.state={users:[]}
+        this.addUser=this.addUser.bind(this);
     }
 
     renderPictures() {
         return this.props.pictures.map((img, index) =>
             <UserBox key={index} index={index} img={img} deleteImg={this.props.deleteImg} />);
+    }
+
+    addUser(user){
+        this.set
     }
 
     render() {
