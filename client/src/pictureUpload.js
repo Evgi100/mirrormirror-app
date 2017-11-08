d3d898a8d5da4c879224a6dc3c4246003b14ea4e
Evgi100 <evgeni.shapira@gmail.com>
2017-11-09 01:10:21 +0200

    Add design new css and html

--- client/src/pictureUpload.js
+++ client/src/pictureUpload.js
@@ -7,25 +7,33 @@ class ImageUpLoader extends React.Component {
     }
     render() {
         return (
-
-            <div className="frame">
-                <div className="center">
-                    <div className="upload-btn input" onClick={this.onDrop}></div>
-
-                    <div className="bar"></div>
-                    <div className="title">Drop file to upload</div>
-                    <div className="dropzone">
-                        <div className="content">
-                            <span className="filename"></span>
-                            <input type="file" className="input"></input>
-                            <div className="upload-btn" >Upload file</div>
-                        </div>
+            <div className="container">
+                <div className="mirrorBox">
+                    <div className="mirror">
+                        <div className="frame"></div>
                     </div>
+                    <div className="upload-btn"></div>
                 </div>
-                <div className="upload-btn input"></div>
             </div>
         );
     }
 }
 
 export default ImageUpLoader;
+
+// Old Html before taking andrea version
+
+{/* <div className="center">
+<div className="upload-btn input" onClick={this.onDrop}></div>
+
+<div className="bar"></div>
+<div className="title">Drop file to upload</div>
+<div className="dropzone">
+    <div className="content">
+        <span className="filename"></span>
+        <input type="file" className="input"></input>
+        <div className="upload-btn" >Upload file</div>
+    </div>
+</div>
+</div>
+<div className="upload-btn input"></div> */}
