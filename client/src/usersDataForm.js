98b0fcce1892511cfe0390b2ac8c6a68d5003147
Evgi100 <evgeni.shapira@gmail.com>
2017-11-10 12:25:14 +0200

    First commit working on adding/view events from the db

--- client/src/usersDataForm.js
+++ client/src/usersDataForm.js
@@ -5,69 +5,69 @@ class UsersDataForm extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { user: "", event: "", img: "" }
-        this.sendImg = this.sendImg.bind(this)
-        this.uploadImage = this.uploadImage.bind(this)
-        // this.sendUsersData = this.sendUsersData.bind(this)
+        this.state = { name: "", event: "", eventDate: null }
+        // this.sendImg = this.sendImg.bind(this)
+        // this.uploadImage = this.uploadImage.bind(this)
+        this.sendUsersData = this.sendUsersData.bind(this)
     }
 
-    // sendUsersData(event) {
-    //     event.preventDefault();
-    //     axios.post('/events', this.state)
-    //         .then(response => {
-    //             axios.get('/user:/')
-    //             this.props.addUser(response.data)
-    //             this.setState({ name: "", event: "" })
-    //         })
-    //         .catch(error => {
-    //             console.log('Error fetching and parsing data', error);
-    //         });
-    // hmhgjhf
+    sendUsersData(event) {
+        this.setState({ eventDate: new Date() })
+        event.preventDefault();
+        axios.post('/events', this.state)
+            .then(response => {
+                axios.get('/user:/')
+                this.props.addUser(response.data)
+                this.setState({ name: "", event: "" })
+            })
+            .catch(error => {
+                console.log('Error fetching and parsing data', error);
+            });
+    }
+
+    // sendImg(e) {
+    //     let data = new FormData();
+    //     data.append('file', e.target.value);
+    //     this.setState({ [e.target.id]: data })
     // }
 
-    sendImg(e) {
-        let data = new FormData();
-        data.append('file', e.target.value);
-        this.setState({ [e.target.id]: data })
-    }
+    // uploadImage(event) {
+    //     if (this.state.img) {
+    //         let that = this;
+    //         var filepath = this.state.img;
+    //         // let data = new FormData
+    //         // imgdata=
+    //         // data.append("data");
+    //         event.preventDefault();
+    //         axios.post('/photo', filepath, { headers: {
+    //             'accept': 'application/json',
+    //             'Accept-Language': 'en-US,en;q=0.8',
+    //             'Content-Type': `multipart/form-data; boundary=${filepath._boundary}`,
+    //           } })
+    //             .then(function (res) {
+    //                 that.setState({ img: res })
+    //             })
+    //     }
 
-    uploadImage(event) {
-        if (this.state.img) {
-            let that = this;
-            var filepath = this.state.img;
-            // let data = new FormData
-            // imgdata=
-            // data.append("data");
-            event.preventDefault();
-            axios.post('/photo', filepath, { headers: {
-                'accept': 'application/json',
-                'Accept-Language': 'en-US,en;q=0.8',
-                'Content-Type': `multipart/form-data; boundary=${filepath._boundary}`,
-              } })
-                .then(function (res) {
-                    that.setState({ img: res })
-                })
-        }
-
-    }
+    // }
 
 
 
     render() {
         return (
             <div className="right" >
-                <input className="bubble" placeholder="Name" type="text" value={this.state.name} onChange={(event) => this.setState({ user: event.target.value })}></input>
+                <input className="bubble" placeholder="Name" type="text" value={this.state.name} onChange={(event) => this.setState({ name: event.target.value, })}></input>
                 <input className="bubble" placeholder="Occasion" type="text" value={this.state.event} onChange={(event) => this.setState({ event: event.target.value })}></input>
                 {/* <form id="uploadForm" encType="multipart/form-data" action="/photo" method="post">
                     <input type="file" name="userPhoto" />
                     <input type="submit" value="Upload Image" name="submit" />
                 </form> */}
                 <form id="uploadForm" encType="multipart/form-data" className="buttonWrap">
-                    <input type="file" id="img"  className="action-button animate blue" onChange={this.sendImg} />
-                    <button className="action-button animate blue" onClick={this.uploadImage}>Send</button>
-                    
+                    {/* <input type="file" id="img" className="action-button animate blue" onChange={this.sendImg} /> */}
+                    <button type="button" className="action-button animate blue" onClick={this.sendUsersData}>Send</button>
+
                 </form>
-                < img src={this.state.img} />
+                {/* < img src={this.state.img} /> */}
             </div>
 
         );
