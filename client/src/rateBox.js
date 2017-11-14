8f1b6bdff67d13183eba430273a2ecf74e96d2de
Evgi100 <evgeni.shapira@gmail.com>
2017-11-14 23:49:26 +0200

    Add stars rating desing

--- client/src/rateBox.js
+++ client/src/rateBox.js
@@ -5,71 +5,87 @@ class RateBox extends React.Component {
 
     constructor(props) {
         super(props);
-
-        this.state = { rating: '' }
-        this.handleOnChange = this.handleOnChange.bind(this)
+        this.state = { rateOutfit_1: '',rateOutfit_2: '',rateOutfit_3: '',rateOutfit_4: '' }
+        this.handleOnChange=this.handleOnChange.bind(this)
+        this.handleOnChange1=this.handleOnChange1.bind(this)
+        this.handleOnChange2=this.handleOnChange2.bind(this)
+        this.jquery=this.jquery.bind(this)
     }
 
     handleOnChange(e) {
         this.setState({
-            rating: e.target.value
-        }, ()=> {
+            rateOutfit_1: e.target.value
+        }, ()=> { console.log(this.state)
         })
     }
 
+    handleOnChange1(e) {
+        this.setState({
+            rateOutfit_2: e.target.value
+        }, ()=> { console.log(this.state)
+        })
+    }
 
-    
+    handleOnChange2(e) {
+        this.setState({
+            rateOutfit_3: e.target.value
+        }, ()=> { console.log(this.state)
+        })
+    }
 
+    handleOnChange3(e) {
+        this.setState({
+            rateOutfit_4: e.target.value
+        }, ()=> { console.log(this.state)
+        })
+    }
+jquery(){
+    $("label").click(function(){
+        $(this).parent().find("label").css({"background-color": "#D8D8D8"});
+        $(this).css({"background-color": "#7ED321"});
+        $(this).nextAll().css({"background-color": "#7ED321"});
+      });
+}
+   
     render() {
+ 
         return (
+           
                   <div className="ImageContainerHome">
             <div className="ImageRatingBox">
                 <div className="ImageBoxHome first"></div>
-                <fieldset className="rating">
-                    <input type="radio" id="star5" name="rating" value="5" />
-                    <input type="radio" id="star4" name="rating" value="4" />
-                    <label htmlFor="star4" title="Pretty good">4 stars</label>
-                    <input type="radio" id="star3" name="rating" value="3" />
-                    <label htmlFor="star3" title="Meh">3 stars</label>
-                    <input type="radio" id="star2" name="rating" value="2" />
-                    <label htmlFor="star2" title="Kinda bad">2 stars</label>
-                    <input type="radio" id="star1" name="rating" value="1" />
-                    <label htmlFor="star1" title="Sucks big time">1 star</label>
-                </fieldset>
+                      <fieldset className="rating" onChange={this.jquery}>
+								<input type="radio" id="field1_star5" name="rating1" value="5" /><label className = "full" htmlFor="field1_star5"></label>
+								<input type="radio" id="field1_star4" name="rating1" value="4" /><label className = "full" htmlFor="field1_star4"></label>
+								<input type="radio" id="field1_star3" name="rating1" value="3" /><label className = "full" htmlFor="field1_star3"></label>
+								<input type="radio" id="field1_star2" name="rating1" value="2" /><label className = "full" htmlFor="field1_star2"></label>
+								<input type="radio" id="field1_star1" name="rating1" value="1" /><label className = "full" htmlFor="field1_star1"></label>
+							</fieldset>
+
             </div>
 
             <div className="ImageRatingBox">
                 <div className="ImageBoxHome second"></div>
                 <fieldset className="rating">
-                    <input type="radio" id="star5" name="rating" value="5" />
-                    <label htmlFor="star5" title="Rocks!">5 stars</label>
-                    <input type="radio" id="star4" name="rating" value="4" />
-                    <label htmlFor="star4" title="Pretty good">4 stars</label>
-                    <input type="radio" id="star3" name="rating" value="3" />
-                    <label htmlFor="star3" title="Meh">3 stars</label>
-                    <input type="radio" id="star2" name="rating" value="2" />
-                    <label htmlFor="star2" title="Kinda bad">2 stars</label>
-                    <input type="radio" id="star1" name="rating" value="1" />
-                    <label htmlFor="star1" title="Sucks big time">1 star</label>
-                </fieldset>
+								<input type="radio" id="field1_star5" name="rating1" value="5" /><label className = "full" htmlFor="field1_star5"></label>
+								<input type="radio" id="field1_star4" name="rating1" value="4" /><label className = "full" htmlFor="field1_star4"></label>
+								<input type="radio" id="field1_star3" name="rating1" value="3" /><label className = "full" htmlFor="field1_star3"></label>
+								<input type="radio" id="field1_star2" name="rating1" value="2" /><label className = "full" htmlFor="field1_star2"></label>
+								<input type="radio" id="field1_star1" name="rating1" value="1" /><label className = "full" htmlFor="field1_star1"></label>
+							</fieldset>
             </div>
 
             <div className="ImageRatingBox">
                 <div className="ImageBoxHome third"></div>
                 <fieldset className="rating">
-                    <input className="star" type="radio" id="star5" name="rating" value="5" />
-                    <label className="star" htmlFor="star5" title="Rocks!">5 stars</label>
-                    <input className="star" type="radio" id="star4" name="rating" value="4" />
-                    <label className="star" htmlFor="star4" title="Pretty good">4 stars</label>
-                    <input className="star" type="radio" id="star3" name="rating" value="3" />
-                    <label className="star" htmlFor="star3" title="Meh">3 stars</label>
-                    <input className="star" type="radio" id="star2" name="rating" value="2" />
-                    <label className="star" htmlFor="star2" title="Kinda bad">2 stars</label>
-                    <input className="star" type="radio" id="star1" name="rating" value="1" />
-                    <label className="star" htmlFor="star1" title="Sucks big time">1 star</label>
-                </fieldset>
+								<input type="radio" id="field1_star5" name="rating1" value="5" /><label className = "full" htmlFor="field1_star5"></label>
+								<input type="radio" id="field1_star4" name="rating1" value="4" /><label className = "full" htmlFor="field1_star4"></label>
+								<input type="radio" id="field1_star3" name="rating1" value="3" /><label className = "full" htmlFor="field1_star3"></label>
+								<input type="radio" id="field1_star2" name="rating1" value="2" /><label className = "full" htmlFor="field1_star2"></label>
+								<input type="radio" id="field1_star1" name="rating1" value="1" /><label className = "full" htmlFor="field1_star1"></label>
+							</fieldset>
             </div>
-
+       
             <div className="imageButtonContainerHome">
                 <div className="picture">
                     <img src="https://queerty-prodweb.s3.amazonaws.com/wp/docs/2014/10/liberace-lee.jpg" className="img-responsive img-thumbnailHome img-circle" />
